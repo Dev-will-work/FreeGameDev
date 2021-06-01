9c2d23910475f6d4a763bd7f81bfa25cc4f97ba5
Ramazan <ramazankka220@gmail.com>
2021-06-01 21:32:57 +0300

    added multyplayer

---
+++ project/Stable_project/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+	PhotonView PV;
+
+	public GameObject controller;
+
+	void Awake()
+	{
+		PV = GetComponent<PhotonView>();
+	}
+
+	void Start()
+	{
+		if (PV.IsMine)
+		{
+			CreateController();
+		}
+	}
+
+	GameObject CreateController()
+	{
+		Transform spawnpoint = SpawnManager.Instance.GetSpawnpoint();
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "ganfaul_m_aure Variant"), spawnpoint.position, spawnpoint.rotation, 0, new object[] { PV.ViewID });
+		return controller;
+	}
+
+	public void Die()
+	{
+		PhotonNetwork.Destroy(controller);
+		CreateController();
+	}
+}
